71381930841b05e04fe2c965f089588176d2e092
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-15 19:31:54 -0700

    using f{var} for string.

---
+++ Iterable-Types/String/StringBasic.py
@@ -0,0 +1,22 @@
+####STRING####
+## string is a list too. so, is also iterable type.
+a = "Hello"
+b = "World"
+print(a + " " + b)
+#result = Hello World
+
+print("Do you know \"Python\" ?")
+#result = Do you know "Python" ?
+
+print("No"*3)
+#result = NoNoNo
+
+print(f"{a} people")
+# esult = Hello people
+
+a = "ABCDEF"
+print(a[1:4] + " Restaurant")
+#result = BCD Restaurant
+
+print(a[-1] + "lower")
+#result = Flower
