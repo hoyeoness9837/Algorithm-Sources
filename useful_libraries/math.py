3f70c5e2783741686fdff133357d349773de5f2a
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-15 22:58:25 -0700

    math is added.

---
+++ useful_libraries/math.py
@@ -0,0 +1,16 @@
+#### math ####
+import math
+print(math.factorial(5))
+# result = 5! = 5*4*3*2*1 = 120
+
+print(math.sqrt(100))
+# result = √100 = 10
+
+print(math.gcd(14, 21))
+# result = 14(1,2,7,14) 21(1,3,7,21) => 7
+
+print(math.floor(math.pi))
+# result = floor(3.1415926...) => 3
+
+print(math.ceil(math.e))
+# result = ceil(2.71828...) => 3
