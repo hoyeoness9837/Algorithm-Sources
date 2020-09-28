030cc0da8c29ee98ce94025acb051b8e43df1fdd
Ho yeon KIM <kim.hoyeon93@gmail.com>
2020-09-27 21:18:25 -0700

    설명

--- problems/greedy/백준문제/1_설탕배달.py
+++ problems/greedy/백준문제/1_설탕배달.py
@@ -18,14 +18,14 @@ else:
 11을 입력하면 첫 조건문에서 만족하지 못하므로 -1이 되는데, 그외에는 괜찮다.
 
 ##정답##
-suger = int(input())
+sugar = int(input())
 bag = 0
-while sugar >= 0 :
-    if sugar % 5 == 0 :  # 5의 배수이면
+while sugar >= 0 : # 봉지에 넣고 남은 설탕수가 0이될때까지,
+    if sugar % 5 == 0 :  # 5의 배수이면 
         bag += (sugar // 5)  # 5로 나눈 몫을 구해야 정수가 됨
         print(bag)
         break
-    sugar -= 3  
-    bag += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
+    sugar -= 3  # 5의 배수가 아니면, 5의 배수가 될 때까지 설탕3개당 봉지 1증가
+    bag += 1   
 else:
   print(-1)
