b894facbc3f21f275bc1c704e1d1345a6f5558c8
Ho yeon KIM <kim.hoyeon93@gmail.com>
2020-09-29 23:50:09 -0700

    new problem

---
+++ problems/greedy/백준문제/위너.py
@@ -0,0 +1,7 @@
+Let us remind you about how the total penalties are calculated for this contest:
+
+When you solve a problem at  minutes,  is added to your penalty, where  is the number of incorrect verdicts (except compile errors) received on that problem.
+If you do not solve a problem before the contest ends, the incorrect verdicts on that problem are not counted as penalties.
+Here is a bad news for all of you: we, the problem setters, are planning to join the competition and solve our own problems!
+
+We know our problems really well, so we can solve all the problems before the contest ends. Furthermore, we can precisely predict how long it takes to solve each problem, and how many incorrect verdicts (except compile errors) we get in each problem. Depending on the order of the problems we solve, our total penalty might differ. What is the minimum penalty if we solve all problems?
